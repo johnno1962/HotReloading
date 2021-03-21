b01b32a6e2c7d567dc5c4d91d6092ccaf489970c
John Holdsworth <github@johnholdsworth.com>
2021-03-21 03:15:39 +0100

    Storyboard injection.

--- Sources/HotReloadingGuts/ClientBoot.mm
+++ Sources/HotReloadingGuts/ClientBoot.mm
@@ -5,7 +5,7 @@
 //  Created by John Holdsworth on 02/24/2021.
 //  Copyright © 2021 John Holdsworth. All rights reserved.
 //
-//  $Id: //depot/HotReloading/Sources/HotReloadingGuts/ClientBoot.mm#17 $
+//  $Id: //depot/HotReloading/Sources/HotReloadingGuts/ClientBoot.mm#19 $
 //
 //  Initiate connection to server side of InjectionIII/HotReloading.
 //
@@ -103,6 +103,28 @@ static struct {
     NSMutableArray *output, *order;
     int orderIndex;
 } remapper;
+
++ (void)my_addMappingFromIdentifier:(NSString *)identifier toObject:(id)object forCoder:(id)coder {
+    //NSLog(@"Map %@ = %@", identifier, object);
+    if(remapper.output && [identifier hasPrefix:@"UpstreamPlaceholder-"]) {
+        if (remapper.inputIndexes)
+            remapper.inputIndexes[identifier] = @([remapper.inputIndexes count]);
+        else
+            [remapper.output addObject:object];
+    }
+    [self my_addMappingFromIdentifier:identifier toObject:object forCoder:coder];
+}
+
++ (id)my_mappedObjectForCoder:(id)decoder withIdentifier:(NSString *)identifier {
+    //NSLog(@"Mapped? %@", identifier);
+    if(remapper.output && [identifier hasPrefix:@"UpstreamPlaceholder-"]) {
+        if (remapper.inputIndexes)
+            [remapper.order addObject:remapper.inputIndexes[identifier] ?: @""];
+        else
+            return remapper.output[[remapper.order[remapper.orderIndex++] intValue]];
+    }
+    return [self my_mappedObjectForCoder:decoder withIdentifier:identifier];
+}
 
 + (BOOL)injectUI:(NSString *)changed {
     static NSMutableDictionary *allOrder;

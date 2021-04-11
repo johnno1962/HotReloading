f6cf683d0ae6d69795072b27b32b2ddeea05d90d
John Holdsworth <github@johnholdsworth.com>
2021-04-11 15:27:31 +0200

    Sync SwiftTrace version via XprobePlugin.

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 //
-//  $Id: //depot/HotReloading/Package.swift#31 $
+//  $Id: //depot/HotReloading/Package.swift#32 $
 //
 
 import PackageDescription
@@ -22,7 +22,7 @@ let package = Package(
         .package(url: "https://github.com/johnno1962/SwiftRegex5",
                  .upToNextMinor(from: "5.2.1")),
         .package(url: "https://github.com/johnno1962/XprobePlugin",
-                 .upToNextMinor(from: "2.2.3")),
+                 .upToNextMinor(from: "2.2.4")),
         .package(url: "https://github.com/johnno1962/Remote",
                  .upToNextMinor(from: "2.1.0")),
     ],

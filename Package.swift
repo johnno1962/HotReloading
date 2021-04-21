98f3b27599309d946a70a81d64763602ad83e1e9
John Holdsworth <github@johnholdsworth.com>
2021-04-21 13:21:24 +0200

    Fix for Xcode 12.5

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 //
-//  $Id: //depot/HotReloading/Package.swift#39 $
+//  $Id: //depot/HotReloading/Package.swift#40 $
 //
 
 import PackageDescription
@@ -24,7 +24,7 @@ let package = Package(
         .package(url: "https://github.com/johnno1962/SwiftRegex5",
                  .upToNextMinor(from: "5.2.1")),
         .package(url: "https://github.com/johnno1962/XprobePlugin",
-                 .upToNextMinor(from: "2.3.0")),
+                 .upToNextMinor(from: "2.3.1")),
         .package(url: "https://github.com/johnno1962/Remote",
                  .upToNextMinor(from: "2.2.0")),
     ],

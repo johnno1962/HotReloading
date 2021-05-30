eef7a321197420dbeba77f5ff6507ff113d41f45
John Holdsworth <github@johnholdsworth.com>
2021-05-30 11:39:31 +0200

    Testing at scale, Resilient classes.

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 //
-//  $Id: //depot/HotReloading/Package.swift#49 $
+//  $Id: //depot/HotReloading/Package.swift#50 $
 //
 
 import PackageDescription
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/johnno1962/SwiftTrace",
-                 .upToNextMinor(from: "7.3.8")),
+                 .upToNextMinor(from: "7.4.0")),
         .package(url: "https://github.com/johnno1962/SwiftRegex5",
                  .upToNextMinor(from: "5.2.1")),
         .package(url: "https://github.com/johnno1962/XprobePlugin",

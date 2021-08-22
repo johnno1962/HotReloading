769a493985a2afb66d717978a700d2b5a79d2aaa
John Holdsworth <github@johnholdsworth.com>
2021-08-22 19:00:15 +0200

    Filter out SwiftUI classes from Xprobe sweep.

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 //
-//  $Id: //depot/HotReloading/Package.swift#63 $
+//  $Id: //depot/HotReloading/Package.swift#64 $
 //
 
 import PackageDescription
@@ -22,7 +22,7 @@ let package = Package(
         .package(url: "https://github.com/johnno1962/SwiftRegex5",
                  .upToNextMinor(from: "5.2.1")),
         .package(url: "https://github.com/johnno1962/XprobePlugin",
-                 .upToNextMinor(from: "2.6.1")),
+                 .upToNextMinor(from: "2.6.2")),
         .package(url: "https://github.com/johnno1962/Remote",
                  .upToNextMinor(from: "2.3.2")),
         .package(url: "https://github.com/johnno1962/DLKit",

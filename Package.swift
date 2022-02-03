c8f01aa0d59f0d24dc275a80dac76d29cd9b41bb
John Holdsworth <github@johnholdsworth.com>
2022-02-03 19:13:02 +0100

    Exclude debugging entries from fast_dladdr.

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 //
 //  Repo: https://github.com/johnno1962/HotReloading
-//  $Id: //depot/HotReloading/Package.swift#114 $
+//  $Id: //depot/HotReloading/Package.swift#115 $
 //
 
 import PackageDescription
@@ -29,7 +29,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/johnno1962/SwiftTrace",
-                 .upToNextMinor(from: "8.1.8")),
+                 .upToNextMinor(from: "8.1.9")),
         .package(name: "SwiftRegex",
                  url: "https://github.com/johnno1962/SwiftRegex5",
                  .upToNextMinor(from: "5.2.1")),

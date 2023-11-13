9932affd573fa88c04da2a2d721ce828695278d2
John Holdsworth <>
2023-11-13 12:38:27 +0100

    Own injection queue on macOS.

--- Sources/HotReloadingGuts/ClientBoot.mm
+++ Sources/HotReloadingGuts/ClientBoot.mm
@@ -5,7 +5,7 @@
 //  Created by John Holdsworth on 02/24/2021.
 //  Copyright © 2021 John Holdsworth. All rights reserved.
 //
-//  $Id: //depot/HotReloading/Sources/HotReloadingGuts/ClientBoot.mm#91 $
+//  $Id: //depot/HotReloading/Sources/HotReloadingGuts/ClientBoot.mm#92 $
 //
 //  Initiate connection to server side of InjectionIII/HotReloading.
 //
@@ -39,7 +39,7 @@ NSString *injectionHost = @"127.0.0.1";
     NSString *socketAddr = @INJECTION_ADDRESS;
     __unused const char *buildPhase = APP_PREFIX"You'll need to be running a recent copy of the InjectionIII.app downloaded from https://github.com/johnno1962/InjectionIII/releases?\n"
     APP_PREFIX"And have typed: defaults write com.johnholdsworth.InjectionIII deviceUnlock any\n";
-    BOOL isVapor = dlsym(RTLD_DEFAULT, VAPOUR_SYMBOL) != nullptr;
+    BOOL isVapor = dlsym(RTLD_DEFAULT, VAPOR_SYMBOL) != nullptr;
 #if !defined(INJECTION_III_APP)
 #if TARGET_IPHONE_SIMULATOR || TARGET_OS_OSX
     BOOL isiOSAppOnMac = false;

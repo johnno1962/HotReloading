20ecbd78e7c43e766cda17deffdd9d4a9d772dec
John Holdsworth <github@johnholdsworth.com>
2024-01-18 14:09:42 +0100

    Clear out SwiftEval.swift a bit.

--- Sources/HotReloading/SwiftEval.swift
+++ Sources/HotReloading/SwiftEval.swift
@@ -5,7 +5,7 @@
 //  Created by John Holdsworth on 02/11/2017.
 //  Copyright © 2017 John Holdsworth. All rights reserved.
 //
-//  $Id: //depot/HotReloading/Sources/HotReloading/SwiftEval.swift#262 $
+//  $Id: //depot/HotReloading/Sources/HotReloading/SwiftEval.swift#265 $
 //
 //  Basic implementation of a Swift "eval()" including the
 //  mechanics of recompiling a class and loading the new
@@ -213,7 +213,6 @@ public class SwiftEval: NSObject {
     }
     @objc public var injectionNumber = 100
     @objc public var lastIdeProcPath = ""
-    @objc public var slowLogScan = 0.2
 
     var tmpfile: String { URL(fileURLWithPath: tmpDir)
         .appendingPathComponent("eval\(injectionNumber)").path }
@@ -299,68 +298,6 @@ public class SwiftEval: NSObject {
         return (projectFile, logsDir)
     }
 
-    @objc public func rebuild(storyboard: String) throws {
-        let (_, logsDir) = try determineEnvironment(classNameOrFile: storyboard)
-
-        injectionNumber += 1
-
-        // messy but fast
-        guard shell(command: """
-            # search through build logs, most recent first
-            cd "\(logsDir.path.escaping("$"))" &&
-            for log in `ls -t *.xcactivitylog`; do
-                #echo "Scanning $log"
-                /usr/bin/env perl <(cat <<'PERL'
-                    use English;
-                    use strict;
-
-                    # line separator in Xcode logs
-                    $INPUT_RECORD_SEPARATOR = "\\r";
-
-                    # format is gzip
-                    open GUNZIP, "/usr/bin/gunzip <\\"$ARGV[0]\\" 2>/dev/null |" or die;
-
-                    # grep the log until to find codesigning for product path
-                    my $realPath;
-                    while (defined (my $line = <GUNZIP>)) {
-                        if ($line =~ /^\\s*cd /) {
-                            $realPath = $line;
-                        }
-                        elsif (my ($product) = $line =~ m@/usr/bin/ibtool.*? --link (([^\\ ]+\\\\ )*\\S+\\.app)@o) {
-                            print $product;
-                            exit 0;
-                        }
-                    }
-
-                    # class/file not found
-                    exit 1;
-            PERL
-                ) "$log" >"\(tmpfile).sh" && exit 0
-            done
-            exit 1;
-            """) else {
-            throw scriptError("Locating storyboard compile")
-        }
-
-        guard let resources = try? String(contentsOfFile: "\(tmpfile).sh")
-            .trimmingCharacters(in: .whitespaces) else {
-            throw scriptError("Locating product")
-        }
-
-        guard shell(command: """
-            (cd "\(resources.unescape().escaping("$"))" && for i in 1 2 3 4 5; \
-            do if (find . -name '*.nib' -a -newer "\(storyboard)" | \
-            grep .nib >/dev/null); then break; fi; sleep 1; done; \
-            while (ps auxww | grep -v grep | grep "/ibtool " >/dev/null); do sleep 1; done; \
-            for i in `find . -name '*.nib'`; do cp -rf "$i" "\(
-                        Bundle.main.bundlePath)/$i"; done >"\(logfile)" 2>&1)
-            """) else {
-                throw scriptError("Re-compilation")
-        }
-
-        _ = evalError("Copied \(storyboard)")
-    }
-
     public func actualCase(path: String) -> String? {
         let fm = FileManager.default
         if fm.fileExists(atPath: path) {
@@ -405,7 +342,6 @@ public class SwiftEval: NSObject {
             return dylib
         }
 
-        let scanStart = Date.timeIntervalSinceReferenceDate
         guard var (compileCommand, sourceFile) = try
             compileByClass[classNameOrFile] ??
             (longTermCache[classNameOrFile] as? String)
@@ -425,7 +361,6 @@ public class SwiftEval: NSObject {
                 consult: "\(cmdfile)".
                 """)
         }
-        let scanTime = Date.timeIntervalSinceReferenceDate - scanStart
         sourceFile += "" // remove warning
 
         #if targetEnvironment(simulator)
@@ -566,231 +501,14 @@ public class SwiftEval: NSObject {
                             atomically: false)
     }
 
+    // Implementations provided in UnhidingEval.swift
+    func bazelLight(projectRoot: String, recompile sourceFile: String) throws -> String? {
+        throw evalError("No bazel support")
+    }
+
     func bazelLink(in projectRoot: String, since sourceFile: String,
                    compileCommand: String) throws -> String {
-        guard var objects = bazelFiles(under: projectRoot, where: """
-            -newer "\(sourceFile)" -a -name '*.o' | \
-            egrep '(_swift_incremental|_objs)/' | grep -v /external/
-            """) else {
-            throw evalError("Finding Objects failed. Did you actually make a change to \(sourceFile) and does it compile? InjectionIII does not support whole module optimization. (check logfile: \(logfile))")
-        }
-
-        debug(bazelLight, projectRoot, objects)
-        // precendence to incrementally compiled
-        let incremental = objects.filter({ $0.contains("_swift_incremental") })
-        if incremental.count > 0 {
-            objects = incremental
-        }
-
-        #if true
-        // With WMO, which modifies all object files
-        // we need a way to filter them down to that
-        // of the source file and its related module
-        // library to provide shared "hidden" symbols.
-        // We use the related Swift "output_file_map"
-        // for the module name to include its library.
-        if objects.count > 1 {
-            let objectSet = Set(objects)
-            if let maps  = bazelFiles(under: projectRoot,
-                                      where: "-name '*output_file_map*.json'") {
-                let relativePath = sourceFile .replacingOccurrences(
-                    of: projectRoot+"/", with: "")
-                for map in maps {
-                    if let data = try? Data(contentsOf: URL(
-                        fileURLWithPath: projectRoot)
-                        .appendingPathComponent(map)),
-                       let json = try? JSONSerialization.jsonObject(
-                        with: data, options: []) as? [String: Any],
-                       let info = json[relativePath] as? [String: String] {
-                        if let object = info["object"],
-                           objectSet.contains(object) {
-                            objects = [object]
-                            if let module: String =
-                                map[#"(\w+)\.output_file_map"#] {
-                                for lib in bazelFiles(under: projectRoot,
-                                    where: "-name 'lib\(module).a'") ?? [] {
-                                    moduleLibraries.insert(lib)
-                                }
-                            }
-                            break
-                        }
-                    }
-                }
-            } else {
-                _ = evalError("Error reading maps")
-            }
-        }
-        #endif
-
-        objects += moduleLibraries
-        debug(objects)
-
-        try link(dylib: "\(tmpfile).dylib", compileCommand: compileCommand,
-                 contents: objects.map({ "\"\($0)\""}).joined(separator: " "),
-                 cd: projectRoot)
-        return tmpfile
-    }
-
-    func bazelFiles(under projectRoot: String, where clause:  String,
-                    ext: String = "files") -> [String]? {
-        let list = "\(tmpfile).\(ext)"
-        if shell(command: """
-            cd "\(projectRoot)" && \
-            find bazel-out/* \(clause) >"\(list)" 2>>\"\(logfile)\"
-            """), let files = (try? String(contentsOfFile: list))?
-            .components(separatedBy: "\n").dropLast() {
-            return Array(files)
-        }
-        return nil
-    }
-
-    func bazelLight(projectRoot: String, recompile sourceFile: String) throws -> String? {
-        let relativePath = sourceFile.replacingOccurrences(of:
-            projectRoot+"/", with: "")
-        let bazelRulesSwift = projectRoot +
-            "/bazel-out/../external/build_bazel_rules_swift"
-        let paramsScanner = tmpDir + "/bazel.pl"
-        debug(projectRoot, relativePath, bazelRulesSwift, paramsScanner)
-
-        if !sourceFile.hasSuffix(".swift") {
-            throw evalError("Only Swift sources can be standalone injected with bazel")
-        }
-
-        try #"""
-            use JSON::PP;
-            use English;
-            use strict;
-
-            my ($params, $relative) = @ARGV;
-            my $args = join('', (IO::File->new( "< $params" )
-                or die "Could not open response '$params'")->getlines());
-            my ($filemap) = $args =~ /"-output-file-map"\n"([^"]+)"/;
-            my $file_handle = IO::File->new( "< $filemap" )
-                or die "Could not open filemap '$filemap'";
-            my $json_text = join'', $file_handle->getlines();
-            my $json_map = decode_json( $json_text, { utf8  => 1 } );
-
-            if (my $info = $json_map->{$relative}) {
-                $args =~ s/"-(emit-(module|objc-header)-path"\n"[^"]+)"\n//g;
-                my $paramscopy = "$params.copy";
-                my $paramsfile = IO::File->new("> $paramscopy");
-                binmode $paramsfile, ':utf8';
-                $paramsfile->print($args);
-                $paramsfile->close();
-                print "$paramscopy\n$info->{object}\n";
-                exit 0;
-            }
-            # source file not found
-            exit 1;
-            """#.write(toFile: paramsScanner,
-                       atomically: false, encoding: .utf8)
-
-        let errfile = "\(tmpfile).err"
-        guard shell(command: """
-            # search through bazel args, most recent first
-            cd "\(bazelRulesSwift)" 2>"\(errfile)" &&
-            grep module_name_ tools/worker/swift_runner.h >/dev/null 2>>"\(errfile)" ||
-            (git apply -v <<'BAZEL_PATCH' 2>>"\(errfile)" && echo "⚠️ bazel patched, restart app" >>"\(errfile)" && exit 1) &&
-            diff --git a/tools/worker/swift_runner.cc b/tools/worker/swift_runner.cc
-            index 535dad0..19e1a6d 100644
-            --- a/tools/worker/swift_runner.cc
-            +++ b/tools/worker/swift_runner.cc
-            @@ -369,6 +369,11 @@ std::vector<std::string> SwiftRunner::ParseArguments(Iterator itr) {
-                     arg = *it;
-                     output_file_map_path_ = arg;
-                     out_args.push_back(arg);
-            +      } else if (arg == "-module-name") {
-            +        ++it;
-            +        arg = *it;
-            +        module_name_ = arg;
-            +        out_args.push_back(arg);
-                   } else if (arg == "-index-store-path") {
-                     ++it;
-                     arg = *it;
-            @@ -410,12 +415,28 @@ std::vector<std::string> SwiftRunner::ProcessArguments(
-                 ++it;
-               }
-
-            +  auto copy = "/tmp/bazel_"+module_name_+".params";
-            +  unlink(copy.c_str());
-               if (force_response_file_) {
-                 // Write the processed args to the response file, and push the path to that
-                 // file (preceded by '@') onto the arg list being returned.
-                 auto new_file = WriteResponseFile(response_file_args);
-                 new_args.push_back("@" + new_file->GetPath());
-            +
-            +    // patch to retain swiftc arguments file
-            +    link(new_file->GetPath().c_str(), copy.c_str());
-                 temp_files_.push_back(std::move(new_file));
-            +  } else if (FILE *fp = fopen("/tmp/forced_params.txt", "w+")) {
-            +    // alternate patch to capture arguments file
-            +    for (auto &a : args_destination) {
-            +      const char *carg = a.c_str();
-            +      fprintf(fp, "%s\\n", carg);
-            +      if (carg[0] != '@')
-            +          continue;
-            +      link(carg+1, copy.c_str());
-            +      fprintf(fp, "Linked %s to %s\\n", copy.c_str(), carg+1);
-            +    }
-            +    fclose(fp);
-               }
-
-               return new_args;
-            diff --git a/tools/worker/swift_runner.h b/tools/worker/swift_runner.h
-            index 952c593..35cf055 100644
-            --- a/tools/worker/swift_runner.h
-            +++ b/tools/worker/swift_runner.h
-            @@ -153,6 +153,9 @@ class SwiftRunner {
-               // The index store path argument passed to the runner
-               std::string index_store_path_;
-
-            +  // Swift modue name from -module-name
-            +  std::string module_name_ = "Unknown";
-            +
-               // The path of the global index store  when using
-               // swift.use_global_index_store. When set, this is passed to `swiftc` as the
-               // `-index-store-path`. After running `swiftc` `index-import` copies relevant
-            BAZEL_PATCH
-
-            cd "\(projectRoot)" 2>>"\(errfile)" &&
-            for params in `ls -t /tmp/bazel_*.params 2>>"\(errfile)"`; do
-                #echo "Scanning $params"
-                /usr/bin/env perl "\(paramsScanner)" "$params" "\(relativePath)" \
-                >"\(tmpfile).sh" 2>>"\(errfile)" && exit 0
-            done
-            exit 1;
-            """), let returned = (try? String(contentsOfFile: "\(tmpfile).sh"))?
-                                        .components(separatedBy: "\n") else {
-            if let log = try? String(contentsOfFile: errfile), log != "" {
-                throw evalError(log.contains("ls: /tmp/bazel_*.params") ? """
-                    \(log)Response files not available (see: \(cmdfile))
-                    Edit and save a swift source file and restart app.
-                    """ : """
-                    Locating response file failed (see: \(cmdfile))
-                    \(log)
-                    """)
-            }
-            return nil
-        }
-
-        let params = returned[0]
-        _ = evalError("Compiling using parameters from \(params)")
-
-        guard shell(command: """
-                cd "\(projectRoot)" && \
-                chmod +w `find bazel-out/* -name '*.o'`; \
-                xcrun swiftc @\(params) >\"\(logfile)\" 2>&1
-                """) || shell(command: """
-                cd `readlink "\(projectRoot)/bazel-out"`/.. && \
-                chmod +w `find bazel-out/* -name '*.o'`; \
-                xcrun swiftc @\(params) >>\"\(logfile)\" 2>&1
-                """),
-              let compileCommand = try? String(contentsOfFile: params) else {
-            throw scriptError("Recompiling")
-        }
-
-        return try bazelLink(in: projectRoot, since: sourceFile,
-                             compileCommand: compileCommand)
+        throw evalError("No bazel support")
     }
 
     static let quickFiles = getenv("INJECTION_QUICK_FILES").flatMap {

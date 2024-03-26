520465403305fdd3eae20dba37047127ee43040b
John Holdsworth <github@johnholdsworth.com>
2024-03-26 18:31:19 +0100

    Simplify Simplify.

--- Sources/HotReloading/SwiftKeyPath.swift
+++ Sources/HotReloading/SwiftKeyPath.swift
@@ -4,7 +4,7 @@
 //  Created by John Holdsworth on 20/03/2024.
 //  Copyright © 2024 John Holdsworth. All rights reserved.
 //
-//  $Id: //depot/HotReloading/Sources/HotReloading/SwiftKeyPath.swift#22 $
+//  $Id: //depot/HotReloading/Sources/HotReloading/SwiftKeyPath.swift#24 $
 //
 
 import Foundation
@@ -14,33 +14,36 @@ import SwiftTraceGuts
 import HotReloadingGuts
 #endif
 
-private typealias KeyPathFunc = @convention(c) (UnsafeMutableRawPointer,
-                                                UnsafeRawPointer) -> UnsafeRawPointer
+private struct ViewBodyKeyPaths {
+    typealias KeyPathFunc = @convention(c) (UnsafeMutableRawPointer,
+                                            UnsafeRawPointer) -> UnsafeRawPointer
 
-private let keyPathFuncName = "swift_getKeyPath"
-private var save_getKeyPath: KeyPathFunc!
+    static let keyPathFuncName = "swift_getKeyPath"
+    static var save_getKeyPath: KeyPathFunc!
+
+    static var cache = [String: ViewBodyKeyPaths]()
+    var lastOffset = 0
+    var keyPathNumber = 0
+    var recycled = false
+    var keyPaths = [UnsafeRawPointer]()
+}
 
 @_cdecl("hookKeyPaths")
 public func hookKeyPaths() {
-    guard let original = dlsym(SwiftMeta.RTLD_DEFAULT, keyPathFuncName) else {
-        print("⚠️ Could not find original symbol: \(keyPathFuncName)")
+    guard let original = dlsym(SwiftMeta.RTLD_DEFAULT, ViewBodyKeyPaths.keyPathFuncName) else {
+        print("⚠️ Could not find original symbol: \(ViewBodyKeyPaths.keyPathFuncName)")
         return
     }
     guard let replacer = dlsym(SwiftMeta.RTLD_DEFAULT, "injection_getKeyPath") else {
         print("⚠️ Could not find replacement symbol: injection_getKeyPath")
         return
     }
-    save_getKeyPath = autoBitCast(original)
-    var keyPathRebinding = [rebinding(name: strdup(keyPathFuncName),
+    ViewBodyKeyPaths.save_getKeyPath = autoBitCast(original)
+    var keyPathRebinding = [rebinding(name: strdup(ViewBodyKeyPaths.keyPathFuncName),
                                       replacement: replacer, replaced: nil)]
     SwiftInjection.initialRebindings += keyPathRebinding
     _ = SwiftTrace.apply(rebindings: &keyPathRebinding)
 }
-
-private var keyPaths = [String: (offset: Int, keyPath: UnsafeRawPointer)]()
-private var callOffsets = [String: Int]()
-private var callIndexes = [String: Int]()
-private var recycled = false
 
 @_cdecl("injection_getKeyPath")
 public func injection_getKeyPath(pattern: UnsafeMutableRawPointer,
@@ -56,38 +59,22 @@ public func injection_getKeyPath(pattern: UnsafeMutableRawPointer,
         if !callsym.hasSuffix(".body.getter : some") {
             break
         }
-        let offset = caller-info.dli_saddr
-        if let last = callOffsets[callsym] {
-            if offset <= last {
-                callIndexes[callsym] = 0
-                recycled = false
-            }
-        } else {
-            callIndexes[callsym] = 0
-        }
-        callOffsets[callsym] = offset
-        let callIndex = callIndexes[callsym, default: 0]
-//        print(offset, callIndex)
         let callBase = callsym.replacingOccurrences(of: "<.*?>",
             with: "<>", options: .regularExpression) + ".keyPath#"
-        func callKey(shift: Int = 0) -> String {
-            return callBase+"\(callIndex+shift)"
+        var body = ViewBodyKeyPaths.cache[callBase] ?? ViewBodyKeyPaths()
+        let offset = caller-info.dli_saddr
+        if offset <= body.lastOffset {
+            body.keyPathNumber = 0
+            body.recycled = false
         }
-        let keyPath: UnsafeRawPointer
-        if let (_, prev) = keyPaths[callKey()] {
-//            if let (nextset, next) = keyPaths[callKey(shift: 1)],
-//               offset == nextset {
-//                print("Skipping")
-//                callIndex += 1
-//                prev = next
-//            }
-            SwiftInjection.detail("Recycling \(callKey())")
-            recycled = true
-            keyPath = prev
+        body.lastOffset = offset
+//        print(offset, callIndex)
+        if body.keyPathNumber < body.keyPaths.count {
+            SwiftInjection.detail("Recycling \(callBase)\(body.keyPathNumber)")
+            body.recycled = true
         } else {
-            keyPath = save_getKeyPath(pattern, arguments)
-            keyPaths[callKey()] = (offset, keyPath)
-            if recycled {
+            body.keyPaths.append(ViewBodyKeyPaths.save_getKeyPath(pattern, arguments))
+            if body.recycled {
                 SwiftInjection.log("""
                     ⚠️ New key path expression introduced over injection. \
                     This will likely fail and you'll have to restart your \
@@ -95,9 +82,11 @@ public func injection_getKeyPath(pattern: UnsafeMutableRawPointer,
                     """)
             }
         }
+        let keyPath = body.keyPaths[body.keyPathNumber]
+        body.keyPathNumber += 1
+        ViewBodyKeyPaths.cache[callBase] = body
         _ = Unmanaged<AnyKeyPath>.fromOpaque(keyPath).retain()
-        callIndexes[callsym] = callIndex+1
         return keyPath
     }
-    return save_getKeyPath(pattern, arguments)
+    return ViewBodyKeyPaths.save_getKeyPath(pattern, arguments)
 }
